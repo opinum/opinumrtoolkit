a9544106e026895cf61ecf7addf964f09783cd4f
Patrick Watteyne <pwa@opinum.com>
2020-06-12 17:19:54 +0200

    First doc

--- R/basic_consumption.R
+++ R/basic_consumption.R
@@ -1,8 +1,10 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: patri
-# Created on: 12/06/2020
-
+#' Calculate consumption
+#'
+#' This function calculates the consumption based on an index
+#'
+#' @param df The datahub dataframe containing the index
+#' @return The consumption TimeSeries
+#' @export
 basic_consumption <- function(df) {
   list(TimeSeries=data.frame(Dates=df$Dates[-1], Values=diff(df$Values)))
 }
